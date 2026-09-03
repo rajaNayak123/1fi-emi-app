ae390f8da59fbfe4b39afe398e29f5bf8aad2cd9
rajaNayak123 <nayakraja151@gmail.com>
2026-09-03 11:28:47 +0530

    add schema options helper

---
+++ backend/models/schemaOptions.js
@@ -0,0 +1,9 @@
+export const toJSONOptions = {
+  virtuals: true,
+  versionKey: false,
+  transform: (_doc, ret) => {
+    ret.id = ret._id.toString();
+    delete ret._id;
+    return ret;
+  },
+};
